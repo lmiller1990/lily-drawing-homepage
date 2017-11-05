e0143bd1f29f1a0651e056beab05608ec5de2a39
lachlan <lachlan.miller.1990@outlook.com>
2017-11-05 23:12:23 +0900

    First

---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+<<<<<<< HEAD
+      name: 'HelloWorld',
+=======
+      name: 'Hello',
+>>>>>>> 89c6fb18d4640afd6a5e89106b2493b4465d36a3
+      component: HelloWorld
+    }
+  ]
+})
