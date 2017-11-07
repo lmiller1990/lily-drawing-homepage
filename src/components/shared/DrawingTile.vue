d5e67709b6133a99b92b629b6690d0c37fbd843b
lachlan <lachlan.miller.1990@outlook.com>
2017-11-07 23:13:08 +0900

    Improve transition

--- src/components/shared/DrawingTile.vue
+++ src/components/shared/DrawingTile.vue
@@ -1,9 +1,19 @@
 <template>
   <div class="artwork-tile-wrapper" @mouseenter="mouseover" @mouseleave="mouseout">
-    <img class="img" v-show="!mouseIsOver" src="/static/image_1.jpg" style="max-width: 100%;">
-    <div class="placeholder" :style="{ 'height': height }" v-show="mouseIsOver">
-      OMGOMGOMG
-    </div>
+    <img :class="[ mouseIsOver ? 'hidden image' : 'visible image' ]" src="/static/image_1.jpg" style="max-width: 100%;">
+      <div class="placeholder" v-show="mouseIsOver">
+        <div class="artwork-title">
+          "Tokyo Tower"
+        </div>
+        <div class="price-image">
+          <div>
+            High Quality Print: $25
+          </div>
+          <div>
+            Print + Framed: $35
+          </div>
+        </div>
+      </div>
   </div>
 </template>
 
@@ -32,37 +42,13 @@
       DrawingTileImage
     },
 
-    computed: {
-      img2 () {
-        return `/static/image_2.${this.imageLink.ext}`
-      },
-
-      img () {
-        return `${this.imageLink.href}.${this.imageLink.ext}`
-      },
-
-      details () {
-        return this.mouseIsOver ? { 'opacity': '1' } : {}
-      },
-
-      image () {
-        return this.mouseIsOver ? { 'opacity': '0' } : {}
-      }
-    },
-
     methods: {
-      toggle () {
-      },
-
       mouseover () {
-        console.log(this.$el.querySelector('.img').height)
-        this.width = this.$el.querySelector('.img').height
-        console.log('Mouse over')
+        this.height = this.$el.querySelector('.image').height
         this.mouseIsOver = true
       },
 
       mouseout () {
-        console.log('mouse out')
         this.mouseIsOver = false
       }
     }
@@ -70,17 +56,37 @@
 </script>
 
 <style scoped>
+
 .artwork-tile-wrapper {
   border: 1px solid grey;
   width: 100%;
   position: relative;
 }
 
-.hidden {
-  opacity: 0;
+.placeholder {
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  top: 0;
+  padding-top: 20%;
+  height: 60%;
+  width: 100%;
 }
 
-.image {
-  width: 100%;
+.hidden {
+  opacity: 0.1;
+  transition: opacity 0.5s;
+}
+
+.visible {
+  opacity: 1;
+  transition: opacity 0.5s;
+
+}
+
+.price-image {
+  text-align: center;
 }
 </style>
